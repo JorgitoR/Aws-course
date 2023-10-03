9ec1aed616cfcf8b31acee7d33859c86d897ce61
Jorgito <64322008+JorgitoR@users.noreply.github.com>
2023-10-03 00:38:48 +0000

    Update Boostratp.sh

--- ec2/Boostratp.sh
+++ ec2/Boostratp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash  
 yum update -y
 yum install httpd -y
-echo "<html><body><h1>Hello Cloud Gurus</h1></body></html>" >/var/www/html/index.html
+echo "<html><body><h1>Suscribete al canal</h1></body></html>" >/var/www/html/index.html
 systemctl start httpd
 systemctl enable httpd
